f909fcbf50d410c40dfa7bb16403f5434204a09e
julianoCoelho <mwgmaker@gmail.com>
2026-06-16 21:55:54 -0300

    Base do Projeto

---
+++ App.tsx
@@ -0,0 +1,19 @@
+import 'react-native-gesture-handler'
+import { NavigationContainer, StackRouter } from '@react-navigation/native';
+//import { TabRoutes } from './src/routes/TabRoutes';
+//import { DrawerRoutes } from './src/routes/DrawerRoutes';/
+//import { StackRoutes } from './src/routes/StackRoutes';
+//import { GestureHandlerRootView } from 'react-native-gesture-handler';
+
+
+export default function App() {
+  return (
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <NavigationContainer>
+        {/* <TabRoutes /> */}
+       {/* <DrawerRoutes />*/}
+        {/* <StackRoutes /> */}
+      </NavigationContainer>
+    </GestureHandlerRootView>
+  );
+}
---
+++ src/routes/DrawerRoutes.tsx
@@ -0,0 +1,30 @@
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import { Home } from "../pages/Home";
+import { Sobre } from "../pages/Sobre";
+import { Contato } from "../pages/Contato";
+import { CustomDrawer } from "../components/CustomDrawer";
+import { BuscaCep } from "../pages/buscaCep";
+
+const Drawer = createDrawerNavigator();
+
+export const DrawerRoutes = () => {
+  return (
+    <Drawer.Navigator
+      drawerContent={CustomDrawer}
+      screenOptions={{
+        drawerActiveBackgroundColor: 'red',
+        drawerInactiveBackgroundColor: 'gray',
+        drawerActiveTintColor: 'white',
+        drawerInactiveTintColor: 'white',
+        drawerStyle: {
+          backgroundColor: '#cdcdcd',
+        }
+      }}
+    >
+      <Drawer.Screen name="Home" component={Home} />
+      <Drawer.Screen name="Busca Cep" component={BuscaCep} />
+      <Drawer.Screen name="Sobre" component={Sobre} />
+      <Drawer.Screen name="Contato" component={Contato} />
+    </Drawer.Navigator>
+  )
+}
